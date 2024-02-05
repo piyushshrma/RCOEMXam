053c798e9cded26b5c338d3d5cfc0669aac82d6b
Piyush Sharma <141904179+piyushshrma@users.noreply.github.com>
2024-02-05 17:14:50 +0530

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,20 @@
+function navigateToSemester(semesterNumber) {
+    window.location.href = `../semesters/semester${semesterNumber}.html`;
+}
+
+function navigateToExam(examType) {
+    window.location.href = `../exams/${examType}.html`;
+}
+function toggleDarkMode() {
+    const checkbox = document.getElementById('dark-mode');
+    const container = document.getElementById('container');
+    const background = document.querySelector('.background');
+
+    if (checkbox.checked) {
+        container.style.backgroundColor = '#242424';
+        background.style.backgroundColor = '#242424';
+    } else {
+        container.style.backgroundColor = '#fff';
+        background.style.backgroundColor = '#fff';
+    }
+}
